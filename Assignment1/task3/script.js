7a73a1917b5b6963d00c5fc9d96f80a63fc09abe
Haania <pc1336.haania@gmail.com>
2026-05-17 12:03:51 +0500

    Add files via upload

---
+++ Assignment1/task3/script.js
@@ -0,0 +1,13 @@
+function checkMarks(marks){
+    if (marks >= 90) {
+        alert("Excellent");
+    }
+    else if(marks>=80){
+        alert("Good");
+    }
+    else{
+        alert("Try Again");
+    }
+}
+
+checkMarks(85);
